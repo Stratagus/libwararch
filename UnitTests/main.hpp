0600f6cfd438546949e71ad8aeed3dabd6964ea5
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-07-26 21:58:34 -0700

    *Created basic library template

---
+++ UnitTests/main.hpp
@@ -0,0 +1,20 @@
+/*!
+ *  \brief     Unit Test Runner
+ *  \details   The main testrunner for libgrp,
+ *              tests for the different classes are written in there
+ *              label of <classname>Test.h with the same file structure
+ *              as those in the Source/ directory.
+ *  \author    Bradley Clemetson
+ *  \version   0.0.1
+ *  \date      2012 - Present
+ */
+#ifndef libwararchTestMain_H
+#define libwararchTestMain_H
+
+//Main boost include
+#include <boost/test/unit_test.hpp>
+
+//Test the following classes
+#include "APITest/APITest.hpp"
+
+#endif
